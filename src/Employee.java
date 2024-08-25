2d4c6dec5bbfb964c0daf37d249e2bfe3f1e35e1
SFS76 <sfs76@yandex.ru>
2024-08-25 20:00:32 +0400

    commit

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,49 @@
 public class Employee {
+    private static int idGen = 1;
+    private final int id;
+    private final String name;
+    private int department;
+    private float salary;
+
+    public Employee (String name, int department, float salary) {
+        id = idGen ++;
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", department=" + department +
+                ", salary=" + salary +
+                '}';
+    }
 }
+
